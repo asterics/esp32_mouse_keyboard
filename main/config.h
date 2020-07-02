1535f9c426c47cf3de5ae58cf352eb2ab1f9e04b
Benjamin Aigner <beni@asterics-foundation.org>
2020-07-02 19:24:17 +0200

    Using KConfig variables for pin numbers, BT device name.

--- main/config.h
+++ main/config.h
@@ -1,21 +1,31 @@
- 
+
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
 #define MODULE_ID "ESP32miniBT_v0.1"
-#define GATTS_TAG "FLIPMOUSE"
+
+#if CONFIG_MODULE_FLIPMOUSE
+    #define GATTS_TAG "FLipMouse"
+#else
+    #if CONFIG_MODULE_FABI
+        #define GATTS_TAG "FABI"
+    #else
+        #define GATTS_TAG "esp32_mouse_keyboard"
+    #endif
+#endif
+
 #define MAX_BT_DEVICENAME_LENGTH 40
 
-// serial port of monitor and for debugging
-#define CONSOLE_UART_NUM UART_NUM_0
+// serial port of monitor and for debugging (not in KConfig, won't be changed normally)
+#define CONSOLE_UART_NUM 	 UART_NUM_0
 
 // serial port for connection to other controllers
-#define EX_UART_NUM UART_NUM_2
-#define EX_SERIAL_TXPIN      (GPIO_NUM_16)
-#define EX_SERIAL_RXPIN      (GPIO_NUM_17)
+#define EX_UART_NUM     	 CONFIG_MODULE_UART_NR
+#define EX_SERIAL_TXPIN      CONFIG_MODULE_TX_PIN
+#define EX_SERIAL_RXPIN      CONFIG_MODULE_RX_PIN
 
 // indicator LED
-#define INDICATOR_LED_PIN    (GPIO_NUM_5)
+#define INDICATOR_LED_PIN    CONFIG_MODULE_LED_PIN
 
 typedef struct config_data {
     char bt_device_name[MAX_BT_DEVICENAME_LENGTH];
